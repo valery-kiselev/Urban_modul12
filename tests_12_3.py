836a02f430fd95f2b30977c5a233d12c699b819e
valery-kiselev <83948789+valery-kiselev@users.noreply.github.com>
2024-10-29 19:55:23 +0400

    Add files via upload

--- tests_12_3.py
+++ tests_12_3.py
@@ -3,16 +3,24 @@ import runtour
 
 
 class RunnerTest(unittest.TestCase):
+
+    is_frozen = False
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_walk(self):
         r = runtour.Runner('Bob')
         for i in range(10):
             r.walk()
         self.assertEqual(r.distance, 50)
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_run(self):
         r = runtour.Runner('Jhon')
         for i in range(10):
             r.run()
         self.assertEqual(r.distance, 100)
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_challenge(self):
         r = runtour.Runner('Bobik')
         ru = runtour.Runner('Jhonik')
@@ -23,6 +31,8 @@ class RunnerTest(unittest.TestCase):
 
 class TournamentTest(unittest.TestCase):
 
+    is_frozen = True
+
     @classmethod
     def setUpClass(cls):
         cls.all_results = {}
@@ -32,16 +42,19 @@ class TournamentTest(unittest.TestCase):
         self.r2 = runtour.Runner('Андрей', 9)
         self.r3 = runtour.Runner('Ник', 3)
 
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_tour(self):
         tour = runtour.Tournament(90, self.r1, self.r3)
         self.all_results = tour.start()
         self.assertTrue(self.all_results[2], self.r3)
 
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_tour_2(self):
         tour = runtour.Tournament(90, self.r2, self.r3)
         self.all_results = tour.start()
         self.assertTrue(self.all_results[2], self.r3)
 
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_tour_3(self):
         tour = runtour.Tournament(90, self.r1, self.r2, self.r3)
         self.all_results[3] = tour.start()
